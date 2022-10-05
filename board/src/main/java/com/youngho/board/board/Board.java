ef939f3337357a7a23f256917cba0074a7334a8f
Cha-Young-Ho <jkijki1249@gmail.com>
2022-10-05 21:09:01 +0900

    Feature : create board domain

--- board/src/main/java/com/youngho/board/board/Board.java
+++ board/src/main/java/com/youngho/board/board/Board.java
@@ -1,2 +1,27 @@
-package com.youngho.board.board;public class Board {
+package com.youngho.board.board;
+
+import lombok.*;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("chats")
+public class Board {
+
+    @PrimaryKey()
+    private String id;
+
+    private String content;
+
+    private String title;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
 }
--- board/src/main/java/com/youngho/board/board/BoardRepository.java
+++ board/src/main/java/com/youngho/board/board/BoardRepository.java
@@ -1,2 +1,6 @@
-package com.youngho.board.board;public interface BoardRepository {
+package com.youngho.board.board;
+
+import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
+
+public interface BoardRepository extends ReactiveCassandraRepository<Board, String> {
 }
